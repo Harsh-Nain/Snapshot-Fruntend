7134911ba96f7fa92d92cc2f3d4b43cfe9a38fe3
Harsh Nain <naihh952@gmail.com>
2026-02-06 14:48:46 +0530

    change in vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { API_URL } from "../config/api";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: API_URL,
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
