3a4aa10cde7348257cb5d3f0237fc8dd2825d5f9
Harsh Nain <naihh952@gmail.com>
2026-02-19 08:51:15 +0530

    Snapshot Now Lonched

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -243,12 +243,12 @@ export default function Home() {
         <div className="flex flex-col sm:flex-row justify-center w-full h-[89vh] md:h-[100vh] bg-[#fafafa]">
 
             <div class={`fixed z-9999999999 flex w-3/4 h-17 overflow-hidden top-7 ${!alert && "translate-x-120"}  transition duration-300 ease-in-out right-9 bg-white shadow-lg max-w-96 rounded-xl`}>
-                {alert && <> <svg xmlns="http://www.w3.org/2000/svg" height="96" width="16">    <path stroke-linecap="round" stroke-width="2" stroke={alert.err ? "indianred" : "lightgreen"} fill={alert.err ? "indianred" : "lightgreen"} d="M 8 0 Q 4 4.8, 8 9.6 T 8 19.2 Q 4 24, 8 28.8 T 8 38.4 Q 4 43.2, 8 48 T 8 57.6 Q 4 62.4, 8 67.2 T 8 76.8 Q 4 81.6, 8 86.4 T 8 96 L 0 96 L 0 0 Z"    ></path> </svg>
+                {alert && <> <svg xmlns="http://www.w3.org/2000/svg" height="96" width="16"><path stroke-linecap="round" stroke-width="2" stroke={alert.err ? "indianred" : "lightgreen"} fill={alert.err ? "indianred" : "lightgreen"} d="M 8 0 Q 4 4.8, 8 9.6 T 8 19.2 Q 4 24, 8 28.8 T 8 38.4 Q 4 43.2, 8 48 T 8 57.6 Q 4 62.4, 8 67.2 T 8 76.8 Q 4 81.6, 8 86.4 T 8 96 L 0 96 L 0 0 Z"    ></path> </svg>
 
                     <div class="mx-2.5 overflow-hidden w-full">
-                        {alert.err && <p class="mt-1.5 text-xl flex items-center gap-2 font-bold text-[indianred] leading-8 mr-3 overflow-hidden text-ellipsis whitespace-nowrap">    {alert.err} <MdErrorOutline color="red" /></p>}
-                        {alert.success && <p class="flex items-center gap-2 mt-1.5 text-xl font-bold text-green-400 leading-8 mr-3 overflow-hidden text-ellipsis whitespace-nowrap">    {alert.success}<MdOutlineDoneOutline color="lightgreen" /></p>}
-                        <p class="overflow-hidden leading-5 break-all text-zinc-400 max-h-10">    {alert.message} </p>
+                        {alert.err && <p class="mt-1.5 text-md lg:text-xl flex items-center gap-2 font-bold text-[indianred] leading-8 mr-3 overflow-hidden text-ellipsis whitespace-nowrap">{alert.err} <MdErrorOutline color="red" /></p>}
+                        {alert.success && <p class="flex items-center gap-2 mt-1.5 text-md lg:text-xl font-bold text-green-400 leading-8 mr-3 overflow-hidden text-ellipsis whitespace-nowrap">{alert.success}<MdOutlineDoneOutline color="lightgreen" /></p>}
+                        <p class="overflow-hidden leading-5 break-all text-zinc-400 text-sm lg:text-md max-h-10">{alert.message}</p>
                     </div>
 
                     <button onClick={() => setAlert(null)} class="w-16 cursor-pointer focus:outline-none">
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -55,9 +55,9 @@ export default function Login() {
                     {alert && <> <svg xmlns="http://www.w3.org/2000/svg" height="96" width="16"><path stroke-linecap="round" stroke-width="2" stroke={alert.err ? "indianred" : "lightgreen"} fill={alert.err ? "indianred" : "lightgreen"} d="M 8 0 Q 4 4.8, 8 9.6 T 8 19.2 Q 4 24, 8 28.8 T 8 38.4 Q 4 43.2, 8 48 T 8 57.6 Q 4 62.4, 8 67.2 T 8 76.8 Q 4 81.6, 8 86.4 T 8 96 L 0 96 L 0 0 Z"    ></path> </svg>
 
                         <div class="mx-2.5 overflow-hidden w-full">
-                            {alert.err && <p class="mt-1.5 text-xl flex items-center gap-2 font-bold text-[indianred] leading-8 mr-3 overflow-hidden text-ellipsis whitespace-nowrap">    {alert.err} <MdErrorOutline color="red" /></p>}
-                            {alert.success && <p class="flex items-center gap-2 mt-1.5 text-xl font-bold text-green-400 leading-8 mr-3 overflow-hidden text-ellipsis whitespace-nowrap">    {alert.success}<MdOutlineDoneOutline color="lightgreen" /></p>}
-                            <p class="overflow-hidden leading-5 break-all text-zinc-400 max-h-10">{alert.message} </p>
+                            {alert.err && <p class="mt-1.5 text-md lg:text-xl flex items-center gap-2 font-bold text-[indianred] leading-8 mr-3 overflow-hidden text-ellipsis whitespace-nowrap">    {alert.err} <MdErrorOutline color="red" /></p>}
+                            {alert.success && <p class="flex items-center gap-2 mt-1.5 text-md lg:text-xl font-bold text-green-400 leading-8 mr-3 overflow-hidden text-ellipsis whitespace-nowrap">    {alert.success}<MdOutlineDoneOutline color="lightgreen" /></p>}
+                            <p class="overflow-hidden leading-5 break-all text-sm lg:text-md text-zinc-400 max-h-10">{alert.message} </p>
                         </div>
 
                         <button onClick={() => setAlert(null)} class="w-16 cursor-pointer focus:outline-none">
